accd651d8262e386cf595fad05b17e3a55689755
hufuan <fuan_707@163.com>
2021-05-03 15:12:01 +0800

    add case

--- springboot-jsp-jpa/src/main/java/com/pancm/dao/UserDao.java
+++ springboot-jsp-jpa/src/main/java/com/pancm/dao/UserDao.java
@@ -1,6 +1,5 @@
 package com.pancm.dao;
 
-import org.apache.ibatis.annotations.Mapper;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pancm.pojo.User;
@@ -14,7 +13,6 @@ import com.pancm.pojo.User;
 * @author pancm
 * @date 2018年1月9日
  */
-@Mapper
 public interface UserDao extends JpaRepository<User, Long>{
 	
 }
