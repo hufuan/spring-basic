cd823593f29af59f98aacbddbf6ea0ca755319f5
hufuan <fuan_707@163.com>
2021-05-07 15:58:30 +0800

    add case;

--- ipldashboard/src/frontend/src/pages/TeamPage.js
+++ ipldashboard/src/frontend/src/pages/TeamPage.js
@@ -1,23 +1,28 @@
 import {React, useEffect, useState} from 'react'
+import {useParams} from 'react-router-dom';
 import {MatchDetailCard} from "../components/MatchDetailCard";
 import {MatchSmallCard} from "../components/MatchSmallCard";
 
 export const TeamPage = () => {
     const [team, setTeam] = useState({matches: []});
+    const { teamName } = useParams();
     useEffect(
         () => {
-            const fetychMatches = async () => {
-                const response = await fetch('http://localhost:8080/team/Delhi%20Capitals');
+            const fetchMatches = async () => {
+                const response = await fetch(`http://localhost:8080/team/${teamName}`);
                 const data = await response.json();
                 setTeam(data);
             };
-            fetychMatches();
+            fetchMatches();
         }, []
     );
+    if (!team|| !team.teamName) {
+        return <h1> Team not found</h1>
+    }
     return (
         <div className="TeamPage">
         <h1>{team.teamName}</h1>
-        <MatchDetailCard match={team.matches[0]}/>
+        <MatchDetailCard teamName={team.teamName}  match={team.matches[0]}/>
         {team.matches.slice(1).map(match => <MatchSmallCard match={match} />)}
     </div>
 );
